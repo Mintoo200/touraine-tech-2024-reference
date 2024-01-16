527f93280f560dfde9612418602405078bca7e3b
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-01-16 19:10:56 +0100

    feat: Ajoute les icônes d'up et downvote

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { ImageType } from '../ImageType'
 import { ref } from "vue";
+import Upvote from './icons/Upvote.vue';
+import Downvote from './icons/Downvote.vue';
 
 const props = defineProps<{ entry: ImageType }>()
 const votes = ref(props.entry.votes)
@@ -10,15 +12,20 @@ const {description, src} = props.entry
 <template>
   <li class="card">
     <img :src="src" :alt="description">
-    <button @click="() => votes--">Downvote</button>
-    <output>{{votes}}</output>
-    <button @click="() => votes++">Upvote</button>
+    <span class="controls">
+      <button @click="() => votes--"><Downvote aria-label="Downvote" /></button>
+      <output>{{votes}}</output>
+      <button @click="() => votes++"><Upvote aria-label="Upvote" /></button>
+    </span>
   </li>
 </template>
 
 <style>
 .card {
   width: 100%;
+  border: 1px solid var(--color-card-border);
+  padding: var(--spacing-related-items);
+  position: relative;
 }
 
 .card img {
@@ -26,5 +33,39 @@ const {description, src} = props.entry
   display: block;
   object-fit: cover;
   aspect-ratio: 16/9;
+}
+
+.card button {
+  color: inherit;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  padding: 0;
+  height: 100%;
+  width: auto;
+  line-height: 1;
+  vertical-align: middle;
+}
+
+.card button svg {
+  color: inherit;
+  height: 100%;
+  width: auto;
+}
+
+.card output {
+  font-family: monospace;
+  min-width: 4ch;
+  display: inline-block;
+  text-align: center;
+  margin-inline: var(--spacing-related-items);
+}
+
+.card .controls {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  background: var(--color-controls-background);
+  padding-inline: var(--spacing-related-items);
 }
 </style>
