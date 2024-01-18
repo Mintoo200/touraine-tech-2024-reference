70f1c394062728171e382a1ace726d9a6618c2d7
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-01-18 17:10:57 +0100

    feat: Ajoute un menu au header

--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -1,6 +1,11 @@
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
+    <menu>
+      <li><a href="#">About Pixel</a></li>
+      <li><a href="#">Shop</a></li>
+      <li><a href="#">Contact</a></li>
+    </menu>
   </header>
 </template>
 
@@ -8,5 +13,19 @@
 header {
   border-bottom: 1px solid var(--color-header-separator);
   margin-bottom: var(--spacing-unrelated-items);
+  display: grid;
+  grid-auto-flow: column;
+  align-items: center;
+  justify-content: space-between;
+  padding-inline: 15%;
+}
+
+menu {
+  display: flex;
+  list-style: none;
+  gap: var(--spacing-indirectly-related-items);
+}
+menu li > a {
+  text-decoration: none;
 }
 </style>
